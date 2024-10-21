4c1865d914ad45e140e1e97e4f72d5508540cc9f
Renan <renancossenzo@hotmail.com>
2024-10-21 20:23:35 -0300

    Creating untyped Response

--- src/ToDoList.Application/Responses/Response.cs
+++ src/ToDoList.Application/Responses/Response.cs
@@ -43,9 +43,49 @@ public class Response<T> where T : class
             errors: ["An error ocurred during this operation"]);
 }
 
+public class Response
+{
+    private readonly List<string> _errors;
+
+    [JsonIgnore]
+    public ResponseType Type { get; private set; }
+
+    public IEnumerable<string> Errors => _errors;
+
+    [JsonIgnore]
+    public bool IsSuccess => Type == ResponseType.SUCCESS;
+
+    private Response(ResponseType type,
+        IEnumerable<string> errors)
+    {
+        _errors = [];
+        _errors.AddRange(errors);
+        Type = type;
+    }
+
+    public static Response Success() =>
+        new(ResponseType.SUCCESS,
+            errors: []);
+    
+    public static Response ValidationError(
+        IEnumerable<string> errors) =>
+        new(ResponseType.VALIDATION_ERROR,
+            errors);
+
+    public static Response InternalError() =>
+        new(ResponseType.INTERNAL_ERROR,
+            errors: ["An error ocurred during this operation"]);
+
+    public static Response NotFound(
+        IEnumerable<string> errors) =>
+        new(ResponseType.NOT_FOUND,
+            errors);
+}
+
 public enum ResponseType
 {
     SUCCESS,
     VALIDATION_ERROR,
-    INTERNAL_ERROR
+    INTERNAL_ERROR,
+    NOT_FOUND
 }
